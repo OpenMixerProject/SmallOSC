6541fab42af259171ad139f0769be43e4435c4f6
jcurtis <25715542+jcurtis-cc@users.noreply.github.com>
2024-04-01 18:09:03 +1100

    Add Windows support

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if the script is being run on Windows
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  LIBS="-lWs2_32"
+else
+  LIBS=""
+fi
+
 if type "clang" > /dev/null 2>&1; then
   clang *.c -Werror -O0 -g -o small-osc
 else
